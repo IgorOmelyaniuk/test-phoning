70153b9a58c344bb5ba3ef1ecfef0d39346d1da9
Ihar Amelyaniuk <ihar_amelyaniuk@epam.com>
2023-04-17 21:05:02 +0300

    test8

--- index.js
+++ index.js
@@ -41,27 +41,52 @@ if (button) button.addEventListener('click', handleClick);
 
 
 window.addEventListener('load', async () => {
-  console.log(1111111);
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  console.log('stream', stream);
-  alert(stream);
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  // filter on video inputs, and map to query object
-const queries = devices
-.filter(({ kind }) => kind === "videoinput")
-.map(({ deviceId }) => ({ name: "camera", deviceId }));
+//   console.log(1111111);
+//   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+//   console.log('stream', stream);
+//   alert(stream);
+//   const devices = await navigator.mediaDevices.enumerateDevices();
+//   // filter on video inputs, and map to query object
+// const queries = devices
+// .filter(({ kind }) => kind === "videoinput")
+// .map(({ deviceId }) => ({ name: "camera", deviceId }));
 
-const promises = queries.map((queryObj) =>
-navigator.permissions.query(queryObj)
-);
+// const promises = queries.map((queryObj) =>
+// navigator.permissions.query(queryObj)
+// );
 
-try {
-const results = await Promise.all(promises);
-// log the state of each camera
-results.forEach(({ state }, i) => alert(state));
-} catch (error) {
-console.error(error);
-}
+// try {
+// const results = await Promise.all(promises);
+// // log the state of each camera
+// results.forEach(({ state }, i) => alert(state));
+// } catch (error) {
+// console.error(error);
+// }
+})
+
+
+window.addEventListener('load', async() => {
+  navigator.getUserMedia (
+    {
+       video: true,
+    },
+ 
+    // successCallback
+    function(localMediaStream) {
+       video.onloadedmetadata = function(e) {
+          alert(localMediaStream);
+       };
+    },
+ 
+    // errorCallback
+    function(err) {
+     if(err) {
+      console.log('err', err);
+      alert(err);
+       // Explain why you need permission and how to update the permission setting
+     }
+    }
+ );
 })
 
 // navigator.permissions.query({ name: 'camera' }).then(function(permissionStatus){
