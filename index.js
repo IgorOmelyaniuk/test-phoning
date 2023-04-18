218e1cd42e630baa4f891eac17be1e4d78661040
Ihar Amelyaniuk <ihar_amelyaniuk@epam.com>
2023-04-18 09:47:10 +0300

    BP2-1185: add hover with helper text for property

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ function checkIos() {
   return false;
 }
 
+const handleClick = async () => {
+  if (window?.webkit?.messageHandlers?.scanEan === undefined) {
+      alert('Feature not available on this device');
+      return;
+  }
+
+  try {
+      await navigator?.mediaDevices?.getUserMedia({ video: true });
+  } catch {
+      alert('Please unblock your camera to use this feature');
+  }
+
+  window.webkit.messageHandlers.scanEan.postMessage({});
+};
+
 window.barcodeBtnProps = {
   onSelect: () => console.log('select'),
 }
@@ -20,21 +35,27 @@ window.eanScannedSuccess = function (ean) {
 
 const button = document.getElementById('button');
 
-if (button) button.addEventListener('click', async () => {
-  navigator.permissions.query({name:'camera'}).then(function(result) {
-    alert(result.state);
-    if (result.state === 'granted') {
-        //permission has already been granted, no prompt is shown
-    } else if (result.state === 'prompt') {
-       //there's no peristent permission registered, will be showing the prompt
-    } else if (result.state === 'denied') {
-       //permission has been denied
-    }
-});
-  // try {
-  //   await navigator.mediaDevices.getUserMedia({ video: true });
-  //   alert('success');
-  // } catch {
-  //   alert('error');
-  // }
-})
+if (button) button.addEventListener('click', handleClick);
+
+// if (button) button.addEventListener('click', async () => {
+//   navigator.permissions.query({name:'camera'}).then(function(result) {
+//     alert(result.state);
+//     if (result.state === 'granted') {
+//         //permission has already been granted, no prompt is shown
+//     } else if (result.state === 'prompt') {
+//        //there's no peristent permission registered, will be showing the prompt
+//     } else if (result.state === 'denied') {
+//        //permission has been denied
+//     }
+// });
+//   // try {
+//   //   await navigator.mediaDevices.getUserMedia({ video: true });
+//   //   alert('success');
+//   // } catch {
+//   //   alert('error');
+//   // }
+// })
+
+
+
+  
